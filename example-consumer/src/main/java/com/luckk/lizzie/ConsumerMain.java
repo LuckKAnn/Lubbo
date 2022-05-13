e6a70baa62326de3b66ef297058973255db5f7dd
LuckKAnn <1546165200@qq.com>
2022-05-13 15:34:42 +0800

    notes without code

--- example-consumer/src/main/java/com/luckk/lizzie/ConsumerMain.java
+++ example-consumer/src/main/java/com/luckk/lizzie/ConsumerMain.java
@@ -5,6 +5,10 @@ import com.luckk.lizzie.remoting.netty.NettyServer;
 import com.luckk.lizzie.rpc.LubboScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @FileName: ConsumerMain
  * @Author: LuckKun
@@ -14,9 +18,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @LubboScan(scanPackage = "com.luckk.lizzie")
 public class ConsumerMain {
 
+    private static ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+
+
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext cont = new AnnotationConfigApplicationContext(ConsumerMain.class);
         HelloController helloController = (HelloController) cont.getBean("helloController");
         helloController.rpc();
+        //5s执行一次
+        long period = 1L;
+        service.scheduleAtFixedRate(()->{
+            HelloController controller = (HelloController)cont.getBean("helloController");
+            controller.rpc();
+        },0,period, TimeUnit.SECONDS);
+
     }
 }
